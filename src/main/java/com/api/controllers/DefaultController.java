d58bbeae5b14f92cdbc38bbc67bd9e701479980a
rizwan-ishtiaq <34174422+rizwan-ishtiaq@users.noreply.github.com>
2017-12-02 03:41:01 +0300

    Update DefaultController
    To show JSON

--- src/main/java/com/api/controllers/DefaultController.java
+++ src/main/java/com/api/controllers/DefaultController.java
@@ -1,7 +1,7 @@
 package com.api.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,20 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List<String> showHelp() {
-		List<String> l = new ArrayList<>();
-		l.add("Method,Enpoint,Description");
+	public Map<String, String> showHelp() {
+		Map<String, String> m = new HashMap<>();
+		m.put("Enpoint", "Method, Description");
 
-		l.add("GET,/jobs,List all offers");
-		l.add("POST,/jobs,Add new offer");
-		l.add("GET,/jobs/{jobTitle},Return single offer");
+		m.put("/jobs", "GET, List all offers");
+		m.put("/jobs", "POST, Add new offer");
+		m.put("/jobs/{jobTitle}", "GET, Return single offer");
 
-		l.add("GET,/jobs/{jobTitle}/applications,List all application on offer");
-		l.add("POST,/jobs/{jobTitle}/applications,Apply candidate application");
-		l.add("GET,/jobs/{jobTitle}/applications/{candidateEmail},Return single application on offer for candidate");
+		m.put("/jobs/{jobTitle}/applications", "GET, List all application on offer");
+		m.put("/jobs/{jobTitle}/applications", "POST, Apply candidate application");
+		m.put("/jobs/{jobTitle}/applications/{candidateEmail}",
+				"GET, Return single application on offer for candidate");
 
-		l.add("PUT,/jobs/{jobTitle}/applications/{candidateEmail}/change/{status},Change the status of particular job");
-		return l;
+		m.put("/jobs/{jobTitle}/applications/{candidateEmail}/change/{status}",
+				"PUT, Change the status of particular job");
+		return m;
 	}
 
 }
